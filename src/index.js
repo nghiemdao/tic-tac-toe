ceec155888fb2c47d01eed7838b5f800491070c3
nghiemdao <nghiemvandao@gmail.com>
2019-07-26 13:25:47 +0700

    Extract game status to function

--- src/index.js
+++ src/index.js
@@ -47,7 +47,6 @@ class Game extends Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ? "Go to move #" + move : "Go to game start";
@@ -57,24 +56,30 @@ class Game extends Component {
         </li>
       );
     });
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
+
     return (
       <div className="game">
         <div className="game-board">
           <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div>{this.displayStatus(current)}</div>
           <ol>{moves}</ol>
         </div>
       </div>
     );
   }
+
+  displayStatus = board => {
+    const winner = calculateWinner(board.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    return status;
+  };
 }
 
 // ========================================
